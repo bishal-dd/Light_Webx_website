3db8dac21b3b685a6ff0053c2907fae727f99f41
bishal-dd <dhakalbishal930@gmail.com>
2024-03-13 02:00:46 +0600

    added stitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress()],
+  site: 'https://www.lightwebx.com',
+  integrations: [tailwind(), compress(), sitemap()],
 });
